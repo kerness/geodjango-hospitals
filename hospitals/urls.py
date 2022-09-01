15a305dde0c206309c4096fb59cb5e50d9455226
kerness <maciej.bak260@gmail.com>
2022-09-01 23:52:54 +0200

    feat: add CRUD functionality for hospitals

---
+++ hospitals/urls.py
@@ -0,0 +1,8 @@
+from email.mime import base
+from rest_framework.routers import DefaultRouter
+from .views import HospitalViewSet
+
+router = DefaultRouter()
+router.register(prefix='api/v1/hospitals', viewset=HospitalViewSet, basename='hospital')
+
+urlpatterns = router.urls
--- hospitals_api/urls.py
+++ hospitals_api/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+from hospitals.models import Hospital
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('hospitals.urls'))
 ]
 
 admin.site.site_header='Hospitals Admin'
