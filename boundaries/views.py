4e751c21be2109c20c2af1b41041d48d61e85024
kerness <maciej.bak260@gmail.com>
2022-09-05 12:26:03 +0200

    feat: boundaries area

--- boundaries/views.py
+++ boundaries/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
+from django.contrib.gis.db.models.functions import Area
 from .models import Boundary
 from .serializers import BoundarySerializer
 
 class BoundaryViewSet(viewsets.ModelViewSet):
     queryset = Boundary.objects.all()
     serializer_class = BoundarySerializer
+
+    def get_queryset(self):
+        return Boundary.objects.annotate(area=Area('mpoly')).order_by('area')
