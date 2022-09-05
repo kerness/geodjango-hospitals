8977a5cb9461f251961cedd6ffb93a9efaa5a138
kerness <maciej.bak260@gmail.com>
2022-09-05 12:33:27 +0200

    feat: total bed  capacity

--- hospitals/views.py
+++ hospitals/views.py
@@ -1,7 +1,15 @@
 from rest_framework import viewsets
+from django.db.models import Sum
 from .models import Hospital
 from .serializers import HospitalSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class HospitalViewSet(viewsets.ModelViewSet):
     queryset = Hospital.objects.all()
     serializer_class = HospitalSerializer
+
+    @action(detail=False, methods=['get'])
+    def total_bed_capacity(self, request):
+        bed_capacity = Hospital.objects.aggregate(bed_capacity=Sum('beds'))
+        return Response(bed_capacity)
